5bf4868a578fee280e32e79d126d2a7fe20403d2
ShujiOba <shuji365630@gmail.com>
2018-12-12 21:02:13 +0900

    [add] Newsを追加

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,26 +1,28 @@
 <template>
   <div>
     <graphic :imageURLs="imageURLs" :transitionTime="5"></graphic>
+    <news></news>
   </div>
 </template>
 
 <script>
 import Graphic from "./Graphic";
+import News from "./News";
 
 export default {
   name: "Home",
-  components: { Graphic },
+  components: { Graphic, News },
   data() {
     return {
       imageURLs: [
-        "https://i.imgur.com/NNoFNWf.png",
-        "https://i.imgur.com/VW2DYbL.png",
-        "https://i.imgur.com/UZKEjzG.png",
-        "https://i.imgur.com/6EhAM7G.png",
-        "https://i.imgur.com/QOfZ5r8.png",
-        "https://i.imgur.com/Y4BHWJM.png",
-        "https://i.imgur.com/laSSeYN.png",
-        "https://i.imgur.com/HaYuGQi.png"
+        "https://i.imgur.com/NNoFNWf.png"
+        // "https://i.imgur.com/VW2DYbL.png",
+        // "https://i.imgur.com/UZKEjzG.png"
+        // "https://i.imgur.com/6EhAM7G.png",
+        // "https://i.imgur.com/QOfZ5r8.png",
+        // "https://i.imgur.com/Y4BHWJM.png",
+        // "https://i.imgur.com/laSSeYN.png",
+        // "https://i.imgur.com/HaYuGQi.png"
       ]
     };
   }
