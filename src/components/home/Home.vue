5045f081e9c8562f753ed1f188b9bd169b649584
ShujiOba <shuji365630@gmail.com>
2018-12-12 18:50:57 +0900

    [add] Themeを変更

---
+++ src/components/home/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <graphic :imageURLs="imageURLs" :transitionTime="5"></graphic>
+  </div>
+</template>
+
+<script>
+import Graphic from "./Graphic";
+
+export default {
+  name: "Home",
+  components: { Graphic },
+  data() {
+    return {
+      imageURLs: [
+        "https://i.imgur.com/NNoFNWf.png",
+        "https://i.imgur.com/VW2DYbL.png",
+        "https://i.imgur.com/UZKEjzG.png",
+        "https://i.imgur.com/6EhAM7G.png",
+        "https://i.imgur.com/QOfZ5r8.png",
+        "https://i.imgur.com/Y4BHWJM.png",
+        "https://i.imgur.com/laSSeYN.png",
+        "https://i.imgur.com/HaYuGQi.png"
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
-import 'vue-material/dist/theme/default.css'
+import './assets/css/theme.scss'
 
 Vue.config.productionTip = false
 
